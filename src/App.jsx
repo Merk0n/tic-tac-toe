018e7906f2453f3debd9c75209b6819972c609ea
merk0n <127611734+Merk0n@users.noreply.github.com>
2024-03-20 19:15:34 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+            <button className='bg-cyan-400 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3'>
+                ...
+            </button>
+        </>
+    );
+}
+
+export default App;
