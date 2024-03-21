d4150ae1f0f05ecc65f91fe00841555a00aa5775
merk0n <127611734+Merk0n@users.noreply.github.com>
2024-03-21 20:10:57 +0100

    Prepare basic structure and basic styling using tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'primary-text': '#f1f3f5',
+            },
+        },
     },
     plugins: [],
 };
