8a8b4505026573664131c144ebcffa4f04beee17
merk0n <127611734+Merk0n@users.noreply.github.com>
2024-03-22 08:36:58 +0100

    Update styles and button behavior in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import { MdDoneOutline } from 'react-icons/md';
 
 function App() {
     const fieldStyle =
-        'h-28 w-28 lg:h-36 lg:w-36 border border-gray-800 bg-gray-300 text-8xl lg:text-9xl text-center font-semibold cursor-pointer hover:bg-gray-400';
+        'h-28 w-28 lg:h-36 lg:w-36 border border-gray-800 bg-gray-300 text-8xl lg:text-9xl text-center font-semibold cursor-pointer hover:bg-gray-400 active:bg-gray-500';
 
     const inputStyle =
         'w-80 border-2 border-gray-800 px-2 py-1 text-2xl lg:px-4 lg:py-2 lg:text-3xl';
 
     const editButtonStyle =
-        'ml-4 w-14 rounded-lg border-2 border-gray-800 bg-gray-600 px-2 py-1 text-lg text-green-200 lg:mx-auto';
+        'w-32 rounded-lg border-2 border-gray-800 bg-gray-600 px-2 py-1 text-lg text-green-200 hover:bg-gray-500 active:bg-gray-400 lg:mx-auto';
 
     const acceptButtonStyle =
-        'ml-2 rounded-lg border-2 border-gray-800 bg-green-200 p-1';
+        'rounded-lg border-2 border-gray-800 bg-green-200 p-1 hover:bg-green-300 active:bg-green-400';
 
     return (
         <>
             {/* header */}
-            <header className='h- border-b-2 border-gray-800 bg-gray-600 p-5 text-green-200'>
+            <header className='border-b-2 border-gray-800 bg-gray-600 p-5 text-green-200'>
                 <h1 className='text-center text-6xl font-semibold lg:text-8xl'>
                     Tic-Tac-Toe
                 </h1>
@@ -31,8 +31,11 @@ function App() {
                             type='text'
                             placeholder='Player1'
                         />
-                        <div className='mt-1 flex flex-row'>
-                            <button className={editButtonStyle}>Edit</button>
+                        <div className='mt-1 flex flex-row items-center gap-4'>
+                            <span className='ml-4'>O</span>
+                            <button className={editButtonStyle}>
+                                Edit name
+                            </button>
                             <button className={acceptButtonStyle}>
                                 <MdDoneOutline />
                             </button>
@@ -48,8 +51,11 @@ function App() {
                             type='text'
                             placeholder='Player2'
                         />
-                        <div className='mt-1 flex flex-row'>
-                            <button className={editButtonStyle}>Edit</button>
+                        <div className='mt-1 flex flex-row items-center gap-4'>
+                            <span className='ml-4'>X</span>
+                            <button className={editButtonStyle}>
+                                Edit name
+                            </button>
                             <button className={acceptButtonStyle}>
                                 <MdDoneOutline />
                             </button>
@@ -70,13 +76,13 @@ function App() {
                 </div>
                 {/* button reset */}
                 <div className='mx-auto my-4 flex'>
-                    <button className='rounded-lg border-2 border-gray-800 bg-gray-600 px-4 py-3 text-4xl text-green-200 hover:bg-gray-500 lg:px-5 lg:py-4 lg:text-5xl'>
+                    <button className='rounded-lg border-2 border-gray-800 bg-gray-600 px-4 py-3 text-4xl text-green-200 hover:bg-gray-500 active:bg-gray-400 lg:px-5 lg:py-4 lg:text-5xl'>
                         RESET
                     </button>
                 </div>
             </main>
             {/* footer */}
-            <footer className='absolute bottom-0 flex w-full items-center justify-center gap-1 text-lg lg:text-xl'>
+            <footer className='relative inset-x-0 bottom-0 flex w-full items-center justify-center gap-1 text-lg lg:text-xl'>
                 <p>Copyright &copy; 2024 Merk0n</p>
                 <a href='https://github.com/Merk0n/tic-tac-toe'>
                     <FaGithub className='size-5 lg:size-6' />
