21333ce333f9525d26f2e5fd08b9106a9f808074
merk0n <127611734+Merk0n@users.noreply.github.com>
2024-04-05 14:00:00 +0200

    Fixed bug - GameBoard size

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -3,7 +3,7 @@ export default function GameBoard({ onSelectSquare, board }) {
         'h-28 w-28 lg:h-36 lg:w-36 border border-gray-800 bg-gray-300 text-8xl lg:text-9xl text-center font-semibold cursor-pointer hover:bg-gray-400 active:bg-gray-500';
 
     return (
-        <div className='mx-auto my-0 grid h-[340px] w-[340px] grid-cols-3 border-2 border-gray-800 lg:h-[436px] lg:w-[436px]'>
+        <div className='mx-auto my-0 grid  grid-cols-3 border-4 border-gray-800'>
             {board.map((row, rowIndex) =>
                 row.map((playerSymbol, colIndex) => (
                     <button
